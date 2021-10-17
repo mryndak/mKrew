849f2224397ed8f207f00c64a6c53079800589fe
pawel <15343@wp.pl>
2021-10-17 13:00:47 +0200

    first commit

---
+++ src/main/java/pl/mkrew/app/MKrewApplication.java
@@ -0,0 +1,13 @@
+package pl.mkrew.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MKrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MKrewApplication.class, args);
+	}
+
+}
