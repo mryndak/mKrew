96738dd3bc6a3aa4df0a20a8d9c82f0e1d3d2c8d
misiubest <misiubest@interia.pl>
2021-10-20 00:37:12 +0200

    Dodałem dependencies do pom.xml które dodaje obsługę maila. Stworzyłem serwis do wysyłki maila EmailService z metodą sendEmail()

---
+++ src/main/java/pl/mkrew/app/service/EmailService.java
@@ -0,0 +1,25 @@
+package pl.mkrew.app.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    @SneakyThrows // zamienia wyjatek exception na runtime
+    public void sendEmail(String recipient, String subject, String message) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+        mimeMessageHelper.setTo(recipient);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(message);
+        javaMailSender.send(mimeMessage);
+    }
+}
