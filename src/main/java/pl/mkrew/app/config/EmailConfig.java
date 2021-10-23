0457f83924795d6af89ace08d89d95b6ee7f72a5
Mateusz Ryndak <kontakt@mryndak.pl>
2021-10-23 11:21:26 +0200

    [CIRCLE_CI] Fix build project (#5)
    * [CIRCLE_CI] Fix build project

---
+++ src/main/java/pl/mkrew/app/config/EmailConfig.java
@@ -0,0 +1,15 @@
+package pl.mkrew.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
