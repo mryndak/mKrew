c9bafd86aa393fb27787ad912a800830318f8378
Mateusz Ryndak <kontakt@mryndak.pl>
2021-11-21 13:58:07 +0100

    RCKIK Białystok - fix parser

--- src/main/java/pl/mkrew/app/service/parser/RCKIKBialystokParser.java
+++ src/main/java/pl/mkrew/app/service/parser/RCKIKBialystokParser.java
@@ -2,43 +2,55 @@ package pl.mkrew.app.service.parser;
 
 import lombok.SneakyThrows;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import pl.mkrew.app.domain.BloodGroup;
 import pl.mkrew.app.domain.BloodLevel;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class RCKIKBialystokParser implements BloodSuppliesParser{
-
-    Map<String, BloodLevel> bloodLevelMap = Map.of(
-            "zeroBlood", BloodLevel.L_0,
-            "minBlood", BloodLevel.L_25,
-            "midBLood", BloodLevel.L_50,
-            "maxBlood", BloodLevel.L_75,
-            "stopBlood", BloodLevel.L_100
-    );
 
     @Override
     @SneakyThrows
     public Map<BloodGroup, BloodLevel> fetchData(String website) {
+
+        Map<BloodGroup, BloodLevel> data = new LinkedHashMap<>();
 
         Elements bloodBankChart = Jsoup.connect(website)
                 .get()
                 .body()
                 .select("#bloodBankChart");
 
-        Map<BloodGroup, BloodLevel> data = new LinkedHashMap<>();
-        List<String> bloodGroups = bloodBankChart.dataNodes();
-        List<String> bloodLevels = bloodBankChart.select("style")
-                .eachAttr("class");
+        Objects.requireNonNull(bloodBankChart.first()).getElementsByClass("maxBlood")
+                .stream()
+                .map(Element::text)
+                .forEach(bloodGroup -> {
+                    data.put(
+                            BloodGroup.getBloodGroupByName(bloodGroup),
+                            BloodLevel.L_100
+                    );
+                });
 
-        for(int i = 0; i < bloodGroups.size(); i++ ) {
-            BloodGroup group = BloodGroup.getBloodGroupByName(bloodGroups.get(i));
-            BloodLevel level = bloodLevelMap.get(bloodLevels.get(i));
-            data.put(group, level);
-        }
+        Objects.requireNonNull(bloodBankChart.first()).getElementsByClass("midBlood")
+                .stream()
+                .map(Element::text)
+                .forEach(bloodGroup -> {
+                    data.put(
+                            BloodGroup.getBloodGroupByName(bloodGroup),
+                            BloodLevel.L_50
+                    );
+                });
+
+        Objects.requireNonNull(bloodBankChart.first()).getElementsByClass("minBlood")
+                .stream()
+                .map(Element::text)
+                .forEach(bloodGroup -> {
+                    data.put(
+                            BloodGroup.getBloodGroupByName(bloodGroup),
+                            BloodLevel.L_0
+                    );
+                });
 
         return data;
     }
